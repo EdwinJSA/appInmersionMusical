b87ccad4378b465aedfd1d7005135a19ae062318
CupOfCoffe <edwinjsa2021@gmail.com>
2025-05-29 22:17:13 -0600

    :construction: Se actualizo el readme, se añadió la carpeta template y el index de la aplicacion, y se creo una ruta en la carpeta routes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 const pool = require('./config/conexion_db');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.set('views', path.join(__dirname, 'template'));
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-pool.connect()
-  .then(client => {
-    console.log('✅ Conexión exitosa a PostgreSQL');
-    client.release();
-  })
-  .catch(err => {
-    console.error('❌ Error al conectar a PostgreSQL', err.stack);
-  });
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
