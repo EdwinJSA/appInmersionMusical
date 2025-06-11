bb24dab565399b9b2af71f5b6858eef39343b5c5
Marcelo Mendoza <101734383+Chaco97@users.noreply.github.com>
2025-06-10 21:57:27 -0600

    Update consultaEstudiante.js
    Agregando Consulta para Estudiante

---
+++ src/controllers/consultaEstudiante.js
@@ -0,0 +1,33 @@
+const pool = require("../config/conexion_db");
+
+// Obtiene los datos del estudiante por idñ
+const obtenerDatosEstudiante = async (idEstudiante) => {
+  const query = `
+        SELECT 
+            e.id AS idEstudiante,
+            e.nombre,
+            u.username AS correo,
+            e.numeroEstudiante,
+            e.edad,
+            e.direccion,
+            e.fechaNac,
+            e.estado,
+            e.fecInsc,
+            e.tutor,
+            e.nombreTutor,
+            e.celularTutor,
+            e.correoTutor
+        FROM 
+            Estudiante e
+        JOIN 
+            Usuario u ON e.idUsuario = u.id
+        WHERE 
+            u.username = $1
+        LIMIT 1
+    `;
+  const values = [idEstudiante];
+  const result = await pool.query(query, values);
+  return result.rows[0];
+};
+
+module.exports = { obtenerDatosEstudiante };
